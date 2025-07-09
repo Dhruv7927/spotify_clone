82527e983ac055966fdafa5f4e2291d1f58ece3e
Dhruv7927 <dhruv2k19.3000@gmail.com>
2025-07-09 13:27:48 +0530

    updated the login of user creation

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,19 +1,20 @@
 import { User } from '../models/user.model.js';
 
-export const authCallback = async(req, res) => {
-    try{
+export const authCallback = async (req, res, next) => {
+    try {
         const { id, firstName, lastName, imageUrl } = req.body;
 
-        const user = await User.findOne({clerkId: id});
-        if(!user){
-            await User.create({
+        let user = await User.findOne({ clerkId: id });
+        if (!user) {
+            user = new User({
                 clerkId: id,
                 fullName: `${firstName || ""} ${lastName || ""}`.trim(),
                 imageUrl: imageUrl
-            })
+            });
+            await user.save();
         }
         res.status(200).json({ message: "User authenticated successfully", success: true });
-    } catch (error){
+    } catch (error) {
         console.error("Error in authCallback:", error);
         next(error);
     }
